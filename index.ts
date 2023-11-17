53d0d57dd00019a63dbbb3828c8e086d9ad5b802
Roy Anger <roy@royanger.com>
2023-11-17 12:09:01 -0500

    Updated env example and consts in script

--- index.ts
+++ index.ts
@@ -5,14 +5,15 @@ import * as fs from "fs";
 import * as z from "zod";
 import clerkClient from "@clerk/clerk-sdk-node";
 
-const retryDelay = 10_000; // 10 seconds
-const secretKey = process.env.CLERK_SECRET_KEY;
-const importToDev = process.env.IMPORT_TO_DEV_INSTANCE
-if (!secretKey || !importToDev) {
+const DELAY = process.env.DELAY;
+const RETRY_DELAY = process.env.RETRY_DELAY;
+const SECRET_KEY = process.env.CLERK_SECRET_KEY;
+const IMPORT_TO_DEV = process.env.IMPORT_TO_DEV_INSTANCE
+if (!SECRET_KEY || !IMPORT_TO_DEV || !DELAY || !RETRY_DELAY) {
   throw new Error("CLERK_SECRET_KEY is required. Please copy .env.example to .env and add your key.");
 }
 
-if (secretKey.split('_')[1] !== 'live' && importToDev === 'false') {
+if (SECRET_KEY.split('_')[1] !== 'live' && IMPORT_TO_DEV === 'false') {
   throw new Error("The Clerk Secret Key provided is for a development instance. Development instances are limited to 500 users and do not share their userbase with production instances. If you want to import users to your development instance, please set 'IMPORT_TO_DEV_ISNTANCE' in your .env to 'true'.")
 }
 
@@ -81,7 +82,7 @@ async function processUserToClerk(userData: User) {
     // Keep cooldown in case rate limit is reached as a fallback if the thread blocking fails
     if (error.status === 429) {
       console.log(`Waiting for rate limit to reset`);
-      await new Promise((r) => setTimeout(r, retryDelay));
+      await new Promise((r) => setTimeout(r, RETRY_DELAY));
 
       console.log("Retrying");
       // conditional recursion
