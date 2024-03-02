ee654d04606ae51300490117b6173b1b6362fc8c
Roy Anger <roy@royanger.com>
2024-03-02 03:01:16 -0500

    (wip) Added Supabase validator

--- src/validators.ts
+++ src/validators.ts
@@ -22,3 +22,24 @@ export const authjsUserSchema = z.object({
     .optional(),
 });
 
+
+export const supabaseUserSchema = z.object({
+  id: z.string(),
+  email: z.string().email(),
+  firstName: z.string().optional(),
+  lastName: z.string().optional(),
+  encrypted_password: z.string().optional(),
+  passwordHasher: z
+    .enum([
+      "argon2i",
+      "argon2id",
+      "bcrypt",
+      "md5",
+      "pbkdf2_sha256",
+      "pbkdf2_sha256_django",
+      "pbkdf2_sha1",
+      "scrypt_firebase",
+    ])
+    .default('bcrypt'),
+});
+
